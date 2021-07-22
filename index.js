2be66d5187f9f7fc8c59a2ecf42bea1bec755e13
adelina2222 <adelinachiriac@gmail.com>
2021-07-22 13:01:05 +0100

    Add onTick and onComplete to the class

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ class Timer {
 
         if(callbacks) {
             this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
         }
 
         this.startButton.addEventListener("click", this.start);
@@ -27,9 +29,15 @@ class Timer {
     tick = () => {
         if(this.timeRemaining <= 0) {
             this.pause();
+            if(this.onComplete) {
+                this.onComplete();
+            }
         }
         else {
             this.timeRemaining = this.timeRemaining - 1;
+            if(this.onTick) {
+                this.onTick();
+            }
         }
         
     };
@@ -52,10 +60,10 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
         console.log("Timer has started!");
     },
     onTick() {
-
+        console.log("Timer ticked");
     }, 
     onComplete() {
-
+        console.log("Timer has reached 0!");
     }
 });
 
