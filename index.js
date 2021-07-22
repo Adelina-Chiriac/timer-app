41a966be4938d5ead02ae988bc0cd57b590eea20
adelina2222 <adelinachiriac@gmail.com>
2021-07-22 14:43:34 +0100

    Connect circle element to the timer

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
+const circle = document.querySelector("circle");
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+// Fill the circle length by using the calculated perimeter
+circle.setAttribute("stroke-dasharray", perimeter);
+let currentOffset = 0;
+
 const timer = new Timer(durationInput, startButton, pauseButton, {
     onStart() {
         console.log("Timer has started!");
     },
     onTick() {
+        circle.setAttribute("stroke-dashoffset", currentOffset);
+        currentOffset = currentOffset - 50;
         console.log("Timer ticked");
     }, 
     onComplete() {
