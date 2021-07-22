7da773f05768731ca060d7e020acf61bb8fd9077
adelina2222 <adelinachiriac@gmail.com>
2021-07-22 15:02:17 +0100

    Adjust ticking increment
    for a slightly smoother animation

--- timer.js
+++ timer.js
@@ -19,7 +19,7 @@ class Timer {
             this.onStart();
         }
         this.tick();
-        this.interval = setInterval(this.tick, 50);
+        this.interval = setInterval(this.tick, 20);
     };
 
     pause = () => {
@@ -34,7 +34,7 @@ class Timer {
             }
         }
         else {
-            this.timeRemaining = this.timeRemaining - 0.05;
+            this.timeRemaining = this.timeRemaining - 0.02;
             if(this.onTick) {
                 this.onTick();
             }
